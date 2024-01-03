ac846feccd45f738e992a9cf5cba62f23ac9bca2
Yukiniro <yukiniro@hotmail.com>
2024-01-03 22:42:32 +0800

    feat: add githubcorner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import GitHubCorners from "@uiw/react-github-corners";
 
 export default function Home() {
   const [count, setCount] = useState(0);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <GitHubCorners />
       <div className="z-10 max-w-5xl w-full font-mono text-lg text-center">
         <h1>Hello World</h1>
         <Button onClick={() => setCount(count + 1)}>Click Count: {count}</Button>
